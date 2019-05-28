6b56d6528fbdfe43945af6c80073984da502ca2b
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-05-28 01:40:54 +0000

    Loads the first 50 blocks and listens for new ones

---
+++ src/components/Home/CoinOverview/CoinOverview.component.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { Observable } from "rxjs";
+
+// import classes from "./Coin.module.scss"
+
+interface IProps {
+  coinInfo: any;
+}
+
+export const CoinOverview: React.FC<IProps> = ({ coinInfo }) => {
+  const [blocks, setBlocks] = useState<any[]>([]);
+
+  useEffect(() => {
+    const baseUrl = `https://explorer.freshgrlc.net/api/${coinInfo.symbol.toLowerCase()}`;
+    const obs = new Observable<any>(subscriber => {
+      const es = new EventSource(
+        `${baseUrl}/events/subscribe?channels=blocks,keepalive`
+      );
+      (async () => {
+        ((await (await fetch(
+          `${baseUrl}/blocks/?start=-50&limit=50&expand=miner,transactions`
+        )).json()) as any[]).forEach(block => subscriber.next(block));
+        es.onmessage = message => {
+          const data = message.data;
+          if (data.event === "newblock") {
+            subscriber.next(data.data);
+          }
+        };
+        es.onerror = error => subscriber.error(error);
+      })();
+      return () => es.close();
+    });
+    const sub = obs.subscribe({
+      next: data => {
+        setBlocks(blocks => {
+          const slice = blocks.slice();
+
+          if (slice.length === 50) {
+            slice.pop();
+          }
+
+          slice.unshift(data);
+
+          return slice;
+        });
+      }
+    });
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [coinInfo]);
+  return (
+    <ol>
+      {blocks.map(block => (
+        <li key={block.hash}>{block.height}</li>
+      ))}
+    </ol>
+  );
+};
--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import { CoinOverview } from "./CoinOverview/CoinOverview.component";
+
 // import classes from "./Home.module.scss"
 
 export const Home: React.FC = () => {
-  return <div>Hello Home</div>;
+  const coins = [
+    {
+      name: "garlicoin",
+      displayName: "Garlicoin",
+      symbol: "GRLC"
+    },
+    {
+      name: "tuxcoin",
+      displayName: "Tuxcoin",
+      symbol: "TUX",
+    }
+  ];
+  return (
+    <div>
+      {coins.map(coinInfo => (
+        <CoinOverview key={coinInfo.symbol} coinInfo={coinInfo} />
+      ))}
+    </div>
+  );
 };
