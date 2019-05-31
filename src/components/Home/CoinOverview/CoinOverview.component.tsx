2967973373ccb4c47c7887422b0fa27e02c3dd24
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-05-31 20:37:45 +0000

    Restructured EventSource to allow for mempool to be in the same request

--- src/components/Home/CoinOverview/CoinOverview.component.tsx
+++ src/components/Home/CoinOverview/CoinOverview.component.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { Observable, Subscription } from "rxjs";
+import { Subject } from "rxjs";
 
 import { Header } from "./Header/Header.component";
 import { NetworkInfo } from "./NetworkInfo/NetworkInfo.component";
@@ -7,8 +7,10 @@ import { NetworkInfo } from "./NetworkInfo/NetworkInfo.component";
 import { ICoinInfo } from "interfaces/ICoinInfo.interface";
 import { IBlock } from "interfaces/IBlock.interface";
 import { IUnconfirmedTransaction } from "interfaces/ITransaction.interface";
+import { IEventMessage } from "interfaces/IEventMessage.interface";
 
 import classes from "./CoinOverview.module.scss";
+import { useGetData } from "hooks/useGetData.hook";
 
 interface IProps {
   coinInfo: ICoinInfo;
@@ -20,61 +22,61 @@ export const CoinOverview: React.FC<IProps> = ({ coinInfo }) => {
       `https://api.freshgrlc.net/blockchain/${coinInfo.symbol.toLowerCase()}`,
     [coinInfo]
   );
+  const first50Blocks = useGetData<IBlock[]>(
+    `${baseUrl}/blocks/?start=-50&limit=50&expand=miner,transactions`
+  );
+  const firstUnconfirmedTransactions = useGetData<IUnconfirmedTransaction[]>(
+    `${baseUrl}/transactions/?confirmed=false`
+  );
   const [blocks, setBlocks] = useState<IBlock[]>([]);
   const [unconfirmedTransactions, setUnconfirmedTransactions] = useState<
     IUnconfirmedTransaction[]
   >([]);
 
   useEffect(() => {
-    let cancelled = false;
-    let sub: Subscription;
-    (async () => {
-      const blocksNew = ((await (await fetch(
-        `${baseUrl}/blocks/?start=-50&limit=50&expand=miner,transactions`
-      )).json()) as IBlock[]).reverse();
-      if (!cancelled) {
-        setBlocks(blocksNew);
-      }
-      const obs = new Observable<IBlock>((subscriber) => {
-        const es = new EventSource(
-          `${baseUrl}/events/subscribe?channels=blocks,keepalive`
-        );
-        es.onmessage = (message) => {
-          const data = JSON.parse(message.data);
-
-          if (data.channel === "blocks") {
-            subscriber.next(data.data);
-          }
-        };
-        es.onerror = (error) => subscriber.error(error);
-        return function unsubscribe() {
-          es.close();
-        };
-      });
-      sub = obs.subscribe({
-        next: (data) => {
+    if (first50Blocks && firstUnconfirmedTransactions) {
+      setBlocks(first50Blocks);
+      setUnconfirmedTransactions(firstUnconfirmedTransactions);
+      const blocksSubject = new Subject<IBlock>();
+      const unconfirmedTransactionsSubject = new Subject<
+        IUnconfirmedTransaction[]
+      >();
+      const bSub = blocksSubject.subscribe({
+        next: (block) => {
           setBlocks((blocks) => {
             const slice = blocks.slice();
-
-            if (slice.length > 49) {
-              slice.pop();
-            }
-
-            slice.unshift(data);
-
+            slice.pop();
+            slice.unshift(block);
             return slice;
           });
         },
-        error: (error) => console.error(error),
       });
-    })();
-    return () => {
-      if (sub != null) {
-        sub.unsubscribe();
-      }
-      cancelled = true;
-    };
-  }, [baseUrl]);
+      const uSub = unconfirmedTransactionsSubject.subscribe({
+        next: (mempool) => {
+          setUnconfirmedTransactions(mempool);
+        },
+      });
+      const es = new EventSource(
+        `${baseUrl}/events/subscribe?channels=mempool,blocks,keepalive`
+      );
+      es.addEventListener("message", (message) => {
+        const { event, data } = JSON.parse(message.data) as IEventMessage;
+        if (event === "mempoolupdate") {
+          unconfirmedTransactionsSubject.next(
+            (data as unknown) as IUnconfirmedTransaction[]
+          );
+        } else if (event === "newblock") {
+          blocksSubject.next((data as unknown) as IBlock);
+        }
+      });
+
+      return () => {
+        bSub.unsubscribe();
+        uSub.unsubscribe();
+        es.close();
+      };
+    }
+  }, [baseUrl, first50Blocks, firstUnconfirmedTransactions]);
 
   return (
     <div className={classes.overview}>
@@ -84,6 +86,13 @@ export const CoinOverview: React.FC<IProps> = ({ coinInfo }) => {
         coinInfo={coinInfo}
         baseUrl={baseUrl}
       />
+      <ol>
+        {unconfirmedTransactions.map((unconfirmedTransaction) => (
+          <li key={unconfirmedTransaction.txid}>
+            {unconfirmedTransaction.txid}
+          </li>
+        ))}
+      </ol>
       <ol>
         {blocks.map((block) => (
           <li key={block.hash}>{block.height}</li>
---
+++ src/interfaces/IEventMessage.interface.ts
@@ -0,0 +1,8 @@
+import { IBlock } from "./IBlock.interface";
+import { IUnconfirmedTransaction } from "./ITransaction.interface";
+
+export interface IEventMessage {
+  event: string;
+  channel: string;
+  data: IUnconfirmedTransaction | IBlock | null;
+}
