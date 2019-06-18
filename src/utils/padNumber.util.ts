663367995e055ef5affc5d19f1e65b4fe5579038
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-06-18 19:05:18 +0000

    Refactored pad number to not be retarted

--- src/utils/padNumber.util.ts
+++ src/utils/padNumber.util.ts
@@ -1,7 +1,3 @@
 export const padNumber = (num: number): string => {
-  if (num < 10 && num > -1) {
-    return `0${num}`;
-  } else {
-    return num.toString();
-  }
+  return num.toString().padStart(2, "0");
 };
