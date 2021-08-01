207e96f563cdedc9ce69f6eedce7201259912555
Erik van `t Wout <erik@ewnetworks.eu>
2021-08-01 22:26:20 +0200

    * Fix logo in CoinOverview/Header

--- src/components/Home/CoinOverview/Header/Header.component.tsx
+++ src/components/Home/CoinOverview/Header/Header.component.tsx
@@ -8,10 +8,11 @@ interface IProps {}
 
 export const Header: React.FC<IProps> = () => {
     const coinInfo = useContext(CoinInfoContext);
+    const logo = require(`assets/logos/${coinInfo.logo}`).default;
     return coinInfo ? (
         <h1 className={classes.header}>
             <span className={classes.contents}>
-                <img className={classes.img} src={require(`assets/logos/${coinInfo.logo}`)} alt="" />
+                <img className={classes.img} src={logo} alt="" />
                 <span className={classes.text}>{coinInfo.displayName}</span>
             </span>
         </h1>
